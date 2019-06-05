1bde24abfce65ca8c594bc3a2e7c393b6f4eccb5
Chamnap Chhorn <chamnapchhorn@gmail.com>
2019-06-05 23:25:51 +0700

    Iteration

---
+++ iteration/iteration.go
@@ -0,0 +1,10 @@
+package iteration
+
+// repeats a character with a specified number of times.
+func Repeat(character string, repeatCount int) string {
+	var repeated string
+	for i := 0; i < repeatCount; i++ {
+		repeated += character
+	}
+	return repeated
+}
